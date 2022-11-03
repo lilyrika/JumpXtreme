7f096e9402e8fe477fb1ca10f3a2acbfc9c607bb
lilyrika <lilyheatonx@gmail.com>
2022-11-03 23:27:00 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,66 +1,190 @@
 from random import choice, randint
+from math import ceil
 
 #whatshappening
 
 class Player:
-    Points = 19
+    Level = 1
+    Quid = 0
+
     Strength = 1
     Dexterity = 1
-    Speed = 1
-    Speech = 1
+    Endurance = 1
+    Agility = 1
+    Charisma = 1
     Luck = 1
     
     RWeapon = None
     LWeapon = None
 
+    Stamina = 92
+
     def genAttributes():
-        while Player.points > 0:
-            attribute = randint(1, 5)
+        AllocatedPoints = 24
+        while AllocatedPoints > 0:
+            attribute = randint(1, 6)
             if attribute == 1:
                 Player.Strength += 1
             elif attribute == 2:
                 Player.Dexterity += 1
             elif attribute == 3:
-                Player.Speed += 1
+                Player.Endurance += 1
             elif attribute == 4:
-                Player.Speech += 1
+                Player.Agility += 1
             elif attribute == 5:
+                Player.Charisma += 1
+            elif attribute == 6:
                 Player.Luck += 1
-            Player.Points -= 1
+            AllocatedPoints -= 1
 
         Player.printAttributes()
     
     def printAttributes():
         print("Strength:",Player.Strength)
         print("Dexterity:",Player.Dexterity)
-        print("Speed:",Player.Speed)
-        print("Speech:",Player.Speech)
+        print("Endurance:",Player.Endurance)
+        print("Agility:",Player.Agility)
+        print("Charisma:",Player.Charisma)
         print("Luck:",Player.Luck)
-    
-    def assignWeapon(weapon, slot):
-        pass
+
+    def LevelUp():
+        attribute = str(input("Which attribute would you like to level?\n1. Strength\n2. Dexterity"))
+        if attribute == "Strength":
+            Player.Strength += 1
+        elif attribute == "Dexterity":
+            Player.Dexterity += 1
+        elif attribute == "Endurance":
+            Player.Endurance += 1
+        elif attribute == "Agility":
+            Player.Agility += 1
+        elif attribute == "Charisma":
+            Player.Charisma += 1
+        elif attribute == "Luck":
+            Player.Luck += 1
     
 class Weapon:
     scaleDict = {
-        "S" : 1.8,
-        "A" : 1.6,
-        "B" : 1.4,
-        "C" : 1.2,
-        "D" : 1
+        "S" : 2,
+        "A" : 1.8,
+        "B" : 1.6,
+        "C" : 1.4,
+        "D" : 1.2,
+        "E" : 1
     }
 
+    def scaleDamage(self, Attribute):
+        if Attribute != "-":
+            bonusDamage = ceil(self.Damage / 59.4 * 0.146 * Attribute ** 2 * Weapon.scaleDict.get(Attribute) ** 2)
+        else:
+            bonusDamage = 0
+        print(bonusDamage)
+        return bonusDamage
+    def scaleDex():
+        Player.RWeapon.scaleDamage(Player.Dexterity)
+    def scaleStr():
+        Player.RWeapon.scaleDamage(Player.Strength)
+    def calcDamage(Weapon):
+        damage = 0
+        bonusDamage = Weapon.scaleDex()
+        damage += bonusDamage
+        bonusDamage = Weapon.scaleStr()
+        damage += bonusDamage
+        
+        damage = damage * (1 - Enemy.Defence/100)
+        return damage
+
+    def RAttack():
+        damage = Player.calcDamage(Player.RWeapon)
+        Enemy.HP -= damage
+        print("You dealt",damage,"damage with your",Player.RWeapon.name)
+    def LAttack():
+        damage = Player.calcDamage(Player.LWeapon)
+        Enemy.HP -= damage
+        print("You dealt",damage,"damage with your",Player.LWeapon.name)
+
+
+#region Weapons
+
 class KitchenKnife(Weapon):
-    name = "Kitchen Knife"
+    Name = "Kitchen Knife"
+    StrScale = "E"
+    DexScale = "C"
 
     def __init__(self):
-        self.StrScale = "C"
-        self.DexScale = "D"
-        self.Durability = randint(1, 100)
+        self.Level = 1
+        self.Damage = 65
+
+class MetalBar(Weapon):
+    Name = "Metal Bar"
+    StrScale = "D"
+    DexScale = "D"
+
+    def __init__(self):
+        self.Level = 1
+        self.Damage = 140
+
+class Person:
+    names = [
+        "James",
+        "Jimmy"
+        "Robert",
+        "Rob",
+        "Bob"
+        "John",
+        "Michael",
+        "Mike",
+        "David",
+        "Dave",
+        "William",
+        "Liam",
+        "Richard",
+        "Joseph",
+        "Joe",
+
+        "Olivia",
+        "Emma",
+        "Charlotte",
+        "Amelia",
+        "Emily",
+        "Sophia",
+        "Isabella",
+        "Mia",
+        "Ella",
+        "Abigail",
+        "Sofia",
+        "Scarlett",
+        "Lily",
+        "Grace",
+        "Violet",
+    ]
+    weapons = [
+        KitchenKnife(),
+        MetalBar(),
+    ]
+
+    def __init__(self):
+        self.Name = choice(Person.names)
+        self.HP = randint()
+        self.Weapon = choice(Person.weapons)
+        self.Defence = randint(100, 250) / 10
     
+    def printInfo(self):
+        print()
+        print("Name:",self.Name)
+        print("Weapon:",self.Weapon.Name)
+        print("Defence:",str(self.Defence)+"%")
 
-Player.genAttributes()
+class Enemy:
+    pass
 
-    
+#endregion
 
+def Start():
+    Player.genAttributes()
+    Player.RWeapon = KitchenKnife()
+    SillyGuy = Person() 
+    SillyGuy.printInfo()
+
+Start()
 
 
